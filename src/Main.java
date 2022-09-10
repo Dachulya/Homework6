aa0aec562fbce5f3d16f3f3132313d87cdbee93c
Дарья <dnovogodniaya@ya.ru>
2022-09-10 12:18:08 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,23 @@ public class Main {
         for (short i=10; i>=-10;i--){
             System.out.println(i) ;
         }
+        //Task2
+        //Задание1
+        for (short i=1904;i<=2096;i++){
+            if ( i%4==0){
+                System.out.println(i+"год является високосным");
+            }
+        }
+        //Задание2
+        for(byte i=7; i<=98;i+=7){
+            System.out.println(i);
+        }
+        System.out.println("Задание3");
+        //Задание3
+        for(short i=1; i<=512;i+=i){
+            System.out.println(i);
+        }
+
 
 
 
