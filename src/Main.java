70297226f52467127d2ff80a601ec162c4b97419
Дарья <dnovogodniaya@ya.ru>
2022-09-10 11:50:23 +0300

    Task1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task1
+        //задание1
+            for (short i=0; i<=10;i++){
+                System.out.println(i) ;
+
+        }
+            //Задание2
+        for (short i=10; i>=0;i--){
+            System.out.println(i) ;
+
+        }
+        //Задание3
+        System.out.println("Задание3");
+        for (short i=1; i<=17 ;i++){
+            if (i%2==0){
+                System.out.println(i) ;}
+        }
+        //Задание4
+        for (short i=10; i>=-10;i--){
+            System.out.println(i) ;
+        }
+
+
+
+    }
+
+}
